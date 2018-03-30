a6a5555feb6e20b20e6aea20a266c5add6acab80
Igor Petrovic <igor.petrovic.83@gmail.com>
2018-03-30 10:31:42 +0200

    Completed large-hero and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', () => {
+	
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+
+	watch('./app/index.html', () => {
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', () => {
+		gulp.start('cssInject');
+	});	
+});
+
+gulp.task('cssInject', ['styles'],function(){
+	return gulp.src('./app/temp/styles/styles.css')
+			.pipe(browserSync.stream());
+});
